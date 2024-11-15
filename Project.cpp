9b1f163677d2a74fa6b8750e0e8c1519f7a2d18c
JacobBachle <134236516+JacobBachle@users.noreply.github.com>
2024-11-15 18:43:29 +0000

    Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
+const int BOARD_SIZE = 10;
+const int empty = 0;
+const int occupied = 1;
+const int missed = 2; //if board has this as an element, a player choosed to hit a position where it was empty
+const int hit = 3; //if board has this as an element, a player choosed to hit a position where it was occupied
+
+bool gameWin = false;
+int boardPlayer1[BOARD_SIZE][BOARD_SIZE];
+int boardPlayer2[BOARD_SIZE][BOARD_SIZE];
+
+/*
+1 Aircraft Carrier, 5 spots
+1 Battleship, 4 spots
+1 Destroyer, 3 spots
+1 Submarine, 3 spots
+1 Patrol Boat, 2 spots
+*/
+
+void placeShipsPlayer1 () {
+    //
+}
+
+void printPlayer1Board() {
+    //
+}
+
+void placeShipsPlayer2 () {
+    //
+}
+
+void printPlayer2Board() {
+    //
+}
+
+bool isBoardWin(int board[BOARD_SIZE][BOARD_SIZE]) {
+    //Board is a winning board if no occupied elements are left, note occupied elemts are have a value of 1
+    //Board is a winning board if no element in the array is 1
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j] == occupied) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
 int main() {
-    cout << "Hello, World!" << endl;
+    while (gameWin == false) {
+
+        gameWin = true; //TO BE REMOVED, only present so loop is not infinite
+    }
     return 0;
 }
