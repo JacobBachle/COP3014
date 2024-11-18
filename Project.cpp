c1d91abdd1a11f9bf2088f9dd77ccf612c6cf79f
JacobBachle <134236516+JacobBachle@users.noreply.github.com>
2024-11-18 17:24:59 -0500

    Update Project.cpp
    placeShipsPlayer1 method

--- Project.cpp
+++ Project.cpp
@@ -24,8 +24,92 @@ int boardPlayer2[BOARD_SIZE][BOARD_SIZE];
 1 Patrol Boat, 2 spots
 */
 
-void placeShipsPlayer1 () {
-    //
+void placeShipsPlayer1 (int boardPlayer1[BOARD_SIZE][BOARD_SIZE]) {
+    int shipsSize = {5,4,3,3,2};
+    int shipsQuantity = {1, 1, 1, 1, 1};
+
+    for (int i = 0; i < shipsQuantity.size(); i++) {
+        int currShipsSize = shipsSize[i];
+        bool placed = false;
+
+        while (!placed) {
+            cout << "Place ship with size " << shipsSize[i] << endl;
+            cout << "Enter starting row and column. Input should be (0 - " << BOARD_SIZE << "): ";
+            int currRow;
+            int currCol;
+            cin >> row >> col;
+
+            cout << "Enter orientation (0 for up, 1 for right, 2 for down, 3 for left) ";
+            int orientation;
+            cin >> orientation;
+
+            bool valid = false;
+            while (valid == false) {
+                if (orientation == 0) {
+                    if ((currRow - currShipsSize) > 0) {
+                        valid = true;
+                    }
+                }
+                else if (orientation == 1) {
+                    if ((currCol + currShipsSize) < BOARD_SIZE) {
+                        valid = true;
+                    }
+                }
+                else if (orientation == 2) {
+                    if ((currRow + currShipsSize) < BOARD_SIZE) {
+                        valid = true;
+                    }
+                }
+                else if (orientation == 3) {
+                    if ((currCol - currShipsSize) > 0) {
+                        valid = true;
+                    }
+                }
+
+                if (!valid) {
+                    cout << "Input goes out of bound. Would you like to change the orientation (input 1) or change the starting row and column (input 2): ";
+                    int change;
+                    cin >> change;
+                    if (change == 1) {
+                        cout << "Input new orientation (0 for up, 1 for right, 2 for down, 3 for left): "
+                        cin >> orientation;
+                    }
+                    if (change == 2) {
+                        cout << "Input new starting row and column (row column): "
+                        cin >> currRow;
+                        cin >> currCol;
+                    }
+                    if ((change != 1) && (change != 2)) {
+                        cout << "Please input either 1 or 2"
+                        cin >> change;
+                    }
+                }
+                //If reached this point it is assumed that there is a currRow, currCol, and orientation that is valid for the ship size
+                if (orientation == 0) {
+                    for (int i = currRow; i > currRow - currShipsSize; i--) {
+                        boardPlayer1[i][currCol] = 1;
+                    }
+                }
+                if (orientation == 1) {
+                    for (int j = currCol; j < currCol + currShipsSize; j++) {
+                        boardPlayer1[currRow][j] = 1;
+                    }
+                }
+                if (orientation == 2) {
+                    for (int i = currRow; i < currRow + currShipsSize; i++) {
+                        boardPlayer1[i][currCol] = 1;
+                    }
+                }
+                if (orientation == 3) {
+                    for (int j = currCol; j > currCol - currShipsSize; j--) {
+                        boardPlayer1[currRow][j] = 1;
+                    }
+                }
+            }
+            
+
+        }
+    }
 }
 
 void printPlayer1Board() {
